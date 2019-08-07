6f65c3fb533c1bfe247cd08ca582ec68b71fc5d6
sasuki01 <51392397+sasuki01@users.noreply.github.com>
2019-08-07 09:46:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def callback():
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
     if (userText == 'สวัสดี') :
-        sendText(user,'ค่ะ')
+         sendText(user,'ค่ะ')
     elif (userText == 'บาย') :
-        sendText(user,'เจอกันใหม่นะ')
+         sendText(user,'เจอกันใหม่นะ')
     else :
-        sendText(user,'คืออะไรเหรอ
+         sendText(user,'คืออะไรเหรอ')
     
     return '',200
 
