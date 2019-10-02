1657d043bf12abbad1c375124706b55689666ad0
sasuki01 <51392397+sasuki01@users.noreply.github.com>
2019-10-02 10:53:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def callback():
         sendText(user,'เจอกันใหม่นะ')
     else :
         sendText(user,'คืออะไรเหรอ')
-    
     return '',200
+
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
